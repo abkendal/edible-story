41d3b40c1f1839b25925717bbfb7be231d2a740d
abkendal <kendal.adam@mail.com>
2017-03-16 20:17:17 -0400

    Implement 3rd panel movement

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
+function panel1(){
+	$('.panel-1').toggleClass('load');
+};
 $( document ).ready(function() {
+
+
     
-	
+	$('.panel').click(function(){
+		if ($(this).hasClass('active')){
+			setTimeout(panel1, 2000);
+			$(this).toggleClass('active');
+		}
+		else {
+			$(this).toggleClass('active');
+			setTimeout(panel1, 2000);
+		}
+		
+		
+	});
 
 
 });
