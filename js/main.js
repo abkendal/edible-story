7b95aaacdc35bc3b11d8bb71081d6ddf4bf8391b
abkendal <kendal.adam@mail.com>
2017-03-17 09:25:48 -0400

    Fix panel 2 animation

--- js/main.js
+++ js/main.js
@@ -3,18 +3,25 @@ $( document ).ready(function() {
 
     
 	$('.panel').click(function(){
-		if ($(this).hasClass('active')){
-			$(this).removeClass('load').delay(2500).queue(function(){
-			    $(this).removeClass('active').dequeue();
-			});
+		
+		// 2 step panel animations
+		if ($(this).hasClass('2step')){
+			if ($(this).hasClass('active')){
+				$(this).removeClass('load').delay(2500).queue(function(){
+				    $(this).removeClass('active').dequeue();
+				});
+			}
+			else {
+				$(this).addClass('active').delay(4000).queue(function(){
+				    $(this).addClass('load').dequeue();
+				});
+			}
 		}
+
+		// Single step panel animations
 		else {
-			$(this).addClass('active').delay(4000).queue(function(){
-			    $(this).addClass('load').dequeue();
-			});
+			$(this).toggleClass('active');
 		}
-		
-		
 	});
 
 
