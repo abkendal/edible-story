ad7370340e3ff786f4e1d5b25125af06f4bdaea1
abkendal <kendal.adam@mail.com>
2017-03-16 21:13:50 -0400

    Get pabel 3 video to work and smooth out animations

--- js/main.js
+++ js/main.js
@@ -1,18 +1,57 @@
-function panel1(){
-	$('.panel-1').toggleClass('load');
-};
 $( document ).ready(function() {
 
 
     
 	$('.panel').click(function(){
+		// panel 1
+		// if ($(this).hasClass('panel-1')){
+		// 	if ($(this).hasClass('active')){
+		// 		$(this).removeClass('load').delay(2500).queue(function(){
+		// 		    $(this).removeClass('active').dequeue();
+		// 		});
+		// 	}
+		// 	else {
+		// 		$(this).addClass('active').delay(3000).queue(function(){
+		// 		    $(this).addClass('load').dequeue();
+		// 		});
+		// 	}
+		// }
+
+		// // panel 2
+		// if ($(this).hasClass('panel-2')){
+		// 	if ($(this).hasClass('active')){
+		// 		setTimeout(panel3, 2000);
+		// 		$(this).toggleClass('active');
+		// 	}
+		// 	else {
+		// 		$(this).toggleClass('active').delay;
+		// 		console.log("test");
+		// 		// setTimeout(panel3, 2000);
+		// 	}
+		// }
+
+		// //  panel 3
+		// if ($(this).hasClass('panel-3')){
+		// 	if ($(this).hasClass('active')){
+		// 		$(this).removeClass('load').delay(2500).queue(function(){
+		// 		    $(this).removeClass('active').dequeue();
+		// 		});
+		// 	}
+		// 	else {
+		// 		$(this).addClass('active').delay(4000).queue(function(){
+		// 		    $(this).addClass('load').dequeue();
+		// 		});
+		// 	}
+		// }
 		if ($(this).hasClass('active')){
-			setTimeout(panel1, 2000);
-			$(this).toggleClass('active');
+			$(this).removeClass('load').delay(2500).queue(function(){
+			    $(this).removeClass('active').dequeue();
+			});
 		}
 		else {
-			$(this).toggleClass('active');
-			setTimeout(panel1, 2000);
+			$(this).addClass('active').delay(4000).queue(function(){
+			    $(this).addClass('load').dequeue();
+			});
 		}
 		
 		
